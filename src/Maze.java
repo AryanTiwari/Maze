82b704ca72d02a2cb9668befc77ff62bcb71dc46
AryanTiwari <powerdrgn@gmail.com>
2018-05-12 18:11:35 -0400

    Initial Commit

---
+++ src/Maze.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Maze {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main (String [] args) {
+		
+		double area;
+	
+		System.out.println("Type something");
+		area = sc.nextDouble();
+		
+		
+		System.out.println(area);
+		
+	}
+	
+	
+	
+}
